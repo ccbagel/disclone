d31e750a2fc7ef441db9a32210cf881e6b594adc
ccbagel <hasan.abdi.ahmed97@gmail.com>
2021-08-16 21:30:34 -0600

    finish viewers

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
 import ImageSlider from './ImageSlider.js';
+import Viewers from './Viewers.js';
 
 function Home() {
     return (
         <Container>
             <ImageSlider />
+            <Viewers />
         </Container>
     )
 }
